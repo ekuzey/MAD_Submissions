e455a41aedb48c2a812ad1d829bfe9efe0d4669a
ekuzey <kuzey.erhan@gmail.com>
2013-10-20 18:44:20 +0300

    first commit

---
+++ bil422/assignment1/src/gen/tr/edu/hacettepe/cs/bbm422/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package tr.edu.hacettepe.cs.bbm422;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
